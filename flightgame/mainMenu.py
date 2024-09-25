390511ac2870216379cb75a2217f200fbb2475fe
Eetu Asseri <eetuasseri@gmail.com>
2024-09-25 14:44:34 +0300

    Add mainMenu
    Currently just a proof of concept.

---
+++ flightgame/mainMenu.py
@@ -0,0 +1,19 @@
+#Temporary, proof of concept
+gameName = "Boeing Simulator 2024"
+print(f"## {gameName} ##\n1) New Session\n2) Load previous session\n3) Credits\n4) Exit")
+
+while True:
+    userInput = input()
+    if userInput == "1":
+        # no code yet
+        print()
+        break
+    elif userInput == "2":
+        print()
+    elif userInput == "3":
+        print()
+    elif userInput == "4":
+        exit(1)
+    else:
+        print("Invalid input, retry.")
+print("test")
